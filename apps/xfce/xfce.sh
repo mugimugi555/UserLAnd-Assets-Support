25e8286455aefdf172b6b253e0df4ee4783bf66f
mugimugi <78909330+mugimugi555@users.noreply.github.com>
2022-07-28 15:45:33 +0900

    Update xfce.sh
    If there are changes in the repository, add y to update or the installation will end with a 404 error.

--- apps/xfce/xfce.sh
+++ apps/xfce/xfce.sh
@@ -4,7 +4,7 @@ SCRIPT_PATH=$(realpath ${BASH_SOURCE})
 sudo rm -f $SCRIPT_PATH
 
 if [ ! -f /usr/bin/startxfce4 ]; then
-   sudo apt-get update
+   sudo apt-get update -y
    sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
    if [[ $? != 0 ]]; then
       read -rsp $'An error occurred installing packages, please try again and if it persists provide this log to the developer.\nPress any key to close...\n' -n1 key
